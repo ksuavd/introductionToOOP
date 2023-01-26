c6fb6494873beec2ab0606b865e654d0013a456b
ksuavd <eva2625@mail.ru>
2023-01-26 23:40:22 +0500

    Fraction 26.01

---
+++ Fraction/Fraction.cpp
@@ -0,0 +1,43 @@
+﻿// Fraction.cpp 
+#include <iostream>
+using namespace std;
+class Fraction
+{
+    int integer;//целая часть
+    int numerator;//числитель
+    int denomerator;//знаменатель
+public:
+    int get_integer()const
+    {
+        return integer;
+    }
+    int get_numerator()const
+    {
+        return numerator;
+    }
+    int get_denomerator()const
+    {
+        return denomerator;
+    }
+    void set_integer (int integer)
+    {
+        this->integer= integer;
+    }
+    void set_numerator(int numerator)
+    {
+        this->numerator = numerator;
+    }
+    void set_denomerator(int denomerator)
+    {
+        if (denomerator == 0)denomerator = 1;
+        this->denomerator = denomerator;   
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    cout << "Hello Fraction!\n";
+
+
+}
--- introductionToOOP/introductionToOOP.cpp
+++ introductionToOOP/introductionToOOP.cpp
@@ -1,6 +1,7 @@
 ﻿// introductionToOOP.cpp 
 #include <iostream>
 using namespace std;
+#define delimiter "\n----------------------------------------\n"
 
 class Point //Создавая структуру или класс, мы создаем новый тип данных.
 {
@@ -9,19 +10,19 @@ private:
     double y;
     
 public: 
-    double GetX() const//возвращаем значение переменной х
+    double get_x() const//возвращаем значение переменной х
     {
         return x;
     }
-    void SetX(double valeX)//устанавливаем значение x
+    void set_x(double valeX)//устанавливаем значение x
     {
       this->x = valeX;
     }
-    double GetY() const//возвращаем значение переменной y
+    double get_y() const//возвращаем значение переменной y
     {
         return y;
     }
-    void SetY(double valeY)//устанавливаем значение y
+    void set_y(double valeY)//устанавливаем значение y
     {
         this->y= valeY;//напрямую обращаемся к полям объекта
     }
@@ -29,7 +30,7 @@ public:
     {
         cout << "X = " << x << "\t Y = " << y << endl;
     }*/
-    //Constructors;
+            //Constructors;
    /*  {   Point()
       
         x = y = 0;
@@ -51,37 +52,105 @@ public:
     {
         this->x = x;
         this->y = y;
-    cout << "Constructors:\t" << this << endl;
+
+    cout << "Constructor:\t" << this << endl;
   }
-        
+   Point(const Point& other)
+   {
+       this->x = other.x;
+       this->y = other.y;
+       cout << "CopyConstructors:" << this << endl;
+   }
+
    ~Point()
     {
-        cout << "Constructors:\t" << this << endl;
+        cout << "Destructor:\t" << this << endl;
     }
-    //Metods:
-    double distance (Point dot) const
+         //Operators
+   
+   Point& operator=(const Point& other)
+   {
+       this->x = other.x;
+       this->y = other.y;
+       cout << "CopyAssignment: " << this << endl;
+       return *this;
+   }
+   Point& operator++()
+   {
+       x++;
+       y++;
+       return *this;
+   }
+   Point operator++(int)
+   {
+       Point old = *this;
+       x++;
+       y++;
+       return old;
+   }
+             //Metods:
+   void Print() const
+   {
+       cout << "X = " << x << "\t Y = " << y << endl;
+   }
+    double distance (const Point& dot) const
     {
         double distance_X = this->x - dot.x;
         double distance_Y= this->y - dot.y;
         double distance = sqrt(distance_X * distance_X + distance_Y * distance_Y);
         return distance;
     }
-    void Print() const
+};
+   Point operator+(const Point& left, const Point& right)
     {
-        cout << "X = " << x << "\t Y = " << y << endl;
+        Point res;
+        res.set_x(left.get_x() + right.get_x());
+        res.set_y(left.get_y() + right.get_y());
+        return res;
     }
+   std::ostream& operator<<(std::ostream & os, const Point& obj)
+   {
+   return    os << "X = " << obj.get_x() << "\tY = " << obj.get_y();
+   }
 
-}; 
+   istream& operator >> (istream& is, Point& obj)
+   {
+       double x, y;
+       is >> x >> y;
+       return is;
+   }
+ 
+
+
+    //Comparison operator
+ bool operator ==(const Point& left, const Point& right)
+    {
+      //  if (left.get_x() == right.get_x() && left.get_y() == right.get_y())
+          //  return true;
+        //else
+       //     return false;
+     return(left.get_x() == right.get_x() && left.get_y() == right.get_y());
+    }
+  
+  /*bool operator !=(const Point& left, const Point& right)
+  {
+
+return (!left==right);
+    }*/
 
 //#define STRUCT_POINT
 //#define DISTANCE_CHECK
-double distance(Point A, Point B);
+//#define CONSTRUCTORS_CHECK
+//#define ASSIGNMENT_CHECK_1
+//#define ASSIGNMENT_CHECK_2
+
+double distance(const Point& A, Point& B);
 
 
 int main()
 {
     setlocale(LC_ALL, "");
-    
+
 #ifdef STRUCT_POINT
     A.x = 2;
     A.y = 3;
@@ -90,35 +159,86 @@ int main()
     cout << pA->x << "\t" << pA->y << endl; */
 #endif // STRUCT_POINT
 #ifdef DISTANCE_CHECK
-    Point A;//Объявление переменной А типа Point или же объявление объекта А структуры Point или же создание экземпляра А структуры Point
-    A.SetX(2);
-    A.SetY(3);
-  //  cout << A.GetX() << "\t" << A.GetY() << endl; - для вызова без функции Print, а через cout 
+        Point A;//Объявление переменной А типа Point или же объявление объекта А структуры Point или же создание экземпляра А структуры Point
+    A.set_x(2);
+    A.set_y(3);
+    //  cout << A.get_x() << "\t" << A.get_y() << endl; - для вызова без функции Print, а через cout 
     A.Print();
-
-    Point B; 
-    B.SetX(7);
-    B.SetY(8);
+    cout << delimiter << endl;
+    Point B;
+    B.set_x(7);
+    B.set_y(8);
     B.Print();
-
+    cout << delimiter << endl;
     cout << "Расстояние от точки А до точки В: " << A.distance(B) << endl;
-    cout << "Расстояние между точками А и В: " <<distance(A, B) << endl;
+    cout << delimiter << endl;
+    cout << "Расстояние между точками А и В: " << distance(A, B) << endl;
+    cout << delimiter << endl;
 #endif // DISTANCE_CHECK
+#ifdef CONSTRUCTORS_CHECK
     Point A;
-  //  cout << A.GetX() << "\t" << A.GetY() << endl;
+    //  cout << A.get_x() << "\t" << A.get_y() << endl;
     A.Print();
 
     Point B(2, 3);
-        B.Print();
+    B.Print();
 
-        Point C = 4;// конструктор с 1 параметром
-        C.Print();
+    Point C = 4;// конструктор с 1 параметром
+    C.Print();
+
+    Point D = C;//конструктор копирования
+    D.Print();
+#endif //CONSTRUCTORS_CHECK
+#ifdef ASSIGNMENT_CHECK_1
+    //CopyAssignment
+    Point A(2, 3);
+    A.Print();
+    Point B = A;//Конструктор копирования - CopyConstructor
+    B.Print();
+    Point C;
+    C = B;//Оператор присваивания - CopyAssignment
+    C.Print();
+#endif
+#ifdef ASSIGNMENT_CHECK_2
+    int a, b, c;
+    a = b = c = 0;
+
+    Point A, B, C;
+    cout << delimiter << endl;
+    A = B = C = Point(2, 3);
+    //Point (2,3) - явный вызов конструктора, и этот конструктор создает временный безымяный объект
+    //Временные безымянные объекты существуют в переделах одного выражения.
+    cout << delimiter << endl;
+#endif
+    int a = 2;
+    int b = 3;
+    int c = a + b;
+
+    Point A(2, 3);
+    Point B(4, 5);
+    /* Point C = A + B + A + B;
+     cout << delimiter << endl;
+     C.Print();
+
+     ++C;*/
+     // cout << delimiter << endl;
+    //  C.Print();
+   // cout << (A != B) << endl;
+  //  Point A(2, 3);
+    //A.Print();
+    // A.set_x(12);
+    // A.set_y(13);
+  
+    // A.Print();
+    
+    cout << "Введите координаты точки: "; cin >> A;
+    cout << A << endl;
 }
 
-double distance(Point A, Point B)
+double distance(const Point& A, Point& B)
 {
-    double Xdot = A.GetX() - B.GetX();
-    double Ydot = A.GetY() - B.GetY();
+    double Xdot = A.get_x() - B.get_x();
+    double Ydot = A.get_y() - B.get_y();
     double distance = sqrt(Xdot * Xdot + Ydot * Ydot);
     return distance;
 }
