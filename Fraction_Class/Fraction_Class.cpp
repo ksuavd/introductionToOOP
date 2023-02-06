c5598df86fcf38e177e1d20399c68c95c677fefd
ksuavd <eva2625@mail.ru>
2023-02-06 22:53:55 +0500

    Fraction DONE

--- Fraction_Class/Fraction_Class.cpp
+++ Fraction_Class/Fraction_Class.cpp
@@ -1,6 +1,8 @@
 ﻿// Fraction_Class
 
 #include <iostream>
+#include <string.h>
+
 using namespace std;
 class Fraction;
 Fraction operator*(Fraction left, Fraction right);
@@ -84,12 +86,29 @@ public:
         this->denominator=other.denominator;
         cout << "CopyConstructor:\t" << this << endl;
     }
+    Fraction(double decimal)
+    {
+        decimal += 1e-9;
+        this->integer = decimal; //сохраняем целую часть
+        decimal -= integer; //убираем целую часть из десятичной дроби
+      this->denominator = 1e+9;
+        this->numerator = decimal * denominator;
+        reduce();
+    }
+
+
     //Destructor
     ~Fraction()
     {
         cout << "Destructor:\t\t" << this << endl;
     }
     //Operators
+
+    //Fraction& operator=()(int integer, int numerator, int denominator)
+    //{
+
+    //}
+
     Fraction& operator=(const Fraction& other)//перегрузка оператора присваивания
     {
         this->integer = other.integer;
@@ -138,7 +157,7 @@ public:
         integer = 0;
         return *this;
     }
-    void print()const
+    void print()const //void &print(std::ostream& os)const //для вызова cout из перегруженного оператора
     {
         if (integer)cout << integer;
         if (numerator)
@@ -149,6 +168,7 @@ public:
         }
         else if (integer == 0)cout << 0;
         cout << endl;
+        //return os; //для вызова cout из перегруженного оператора
     }
 
     Fraction& reduce()
@@ -262,19 +282,39 @@ ostream& operator<< (ostream& os, const Fraction& obj)
         os << obj.get_numerator() << "/" << obj.get_denominator();
         if (obj.get_integer())os << ")";
     }
-
     else os << 0; 
     return os;
+    //return obj.print(os); // тоже самое написание перергрузки оператора только короче! 
 }
 istream& operator>> (istream& is, Fraction& obj)
 {
-    int integer;
+  /* int integer;
     int numerator;
     int denominator;
     is >> integer >> numerator >> denominator;
     obj.set_integer(integer);
     obj.set_numerator(numerator);
-    obj.set_denominator(denominator);
+    obj.set_denominator(denominator);*/ 
+    int number[3] = {}; //массив для чисел в дроби
+
+    const int SIZE = 32; // строка
+    char buffer[SIZE] = {};
+    char delimitres [] = "/ ()";
+
+    //is>> buffer;
+    is.getline(buffer, SIZE);
+    int n = 0;// счетчик чисел в введной строке
+    for (char* pch = strtok(buffer, delimitres); pch; pch = strtok(NULL, delimitres))
+        number[n++] = atoi(pch);// функция atoi (...) ASCII-string to integer преобразование 
+    switch (n)
+    {
+        case 1:obj.set_integer(number[0]); break;
+        case 2:
+            obj.set_numerator(number[0]); 
+            obj.set_denominator(number[1]);
+            break;
+        case 3:((number[0]), (number[1]), (number[2])); break;
+    }
     return is;
 }
 
@@ -337,8 +377,8 @@ int main()
     A.print();
     cout << A << endl;
 
-    Fraction B; 
-    cout << "Введите дробь: "; cin >> B; cout << B << endl;
+    /*Fraction B; 
+    cout << "Введите дробь: "; cin >> B; cout << B << endl;*/
 #endif
     //explicit -явный 
     /*
@@ -350,7 +390,7 @@ int main()
 
     */
 
-    Fraction A(2,3,4);
+    /**   Fraction A(2,3,4);
     cout << A << endl;
     int a = (int)A;
     cout << a << endl;
@@ -360,13 +400,13 @@ int main()
     Fraction B(2, 3, 4);
     double b = (double)B;
     cout << b << endl;
-
-    /*
-    Fraction C = 2.75;
+    
+   Fraction C = 2.76;
     cout << C << endl;
     */
-   
-
+    
+    Fraction D;
+    cout << "Введите дробь: "; cin >> D; cout << D << endl; 
     
 
 }
